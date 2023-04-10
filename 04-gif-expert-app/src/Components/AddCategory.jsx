ee1887e7e6f526be398cf49e8d3524ff8777b994
Abraham <abrahamlega@gmail.com>
2023-04-10 22:11:54 +0100

    añadidos componentes y formulario de gifs

---
+++ 04-gif-expert-app/src/Components/AddCategory.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+
+export const AddCategory = () => {
+
+    const [inputValue, setInputValue] = useState('One Punch');
+
+    const onInputChange = ({target}) => {
+        setInputValue(target.value);
+    }
+
+    const onSubmit = (evento) => {
+        event.preventDefault();
+        console.log(inputValue);
+    }
+
+  return (
+    <form onSubmit={(event) => onSubmit(event)}>
+        <input
+            type="text"
+            placeholder="Buscar Gifs"
+            value={inputValue}
+            onChange={onInputChange}
+        />
+    </form>
+    
+  )
+}
--- 04-gif-expert-app/src/GifExpertApp.jsx
+++ 04-gif-expert-app/src/GifExpertApp.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { AddCategory } from './Components/AddCategory';
 
 export const GifExpertApp = () => {
+
+  const [categories, setCategories] = useState([ 'One Punch', 'Dragon Ball' ]);
+
+  const onAddCategory = () => {
+    setCategories(['Valorant', ...categories]);
+  }
+
   return (
     <>
+      {/* Título */ }
         <h1>GifExpertApp</h1>
+
+        { /* Input */ }
+        <AddCategory />
+
+        { /* Tarjetas */ }
+        <button onClick={onAddCategory}>Agregar</button>
+        <ol>
+          { categories.map( category => {
+            return <li key={ category }>{category}</li>
+          })}
+        </ol>
+          { /* GifItem */ }
     </>
   )
 }
