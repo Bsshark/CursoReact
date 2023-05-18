0db481dd30ed6f2314112e64ebc95b38029da927
Abraham <abrahamlega@gmail.com>
2023-05-18 22:38:04 +0100

    125 - Añadidos componentes y refactorizacion

--- 05-hook-app/src/03-examples/MultipleCustomHooks.jsx
+++ 05-hook-app/src/03-examples/MultipleCustomHooks.jsx
@@ -1,15 +1,33 @@
-import React from 'react'
-import { useFetch } from '../hooks/useFetch'
+import { useFetch, useCounter } from "../hooks";
+import { LoadingQuote, Quote } from "./index";
 
 export const MultipleCustomHooks = () => {
 
-    const { data, isLoading, hasError } = useFetch('https://api.breakingbadquotes.xyz/v1/quotes');
+    const {counter, increment, decrement, reset} = useCounter(0);
 
-    console.log(data, isLoading, hasError);
+    const { data, isLoading, hasError } = useFetch('https://api.breakingbadquotes.xyz/v1/quotes/10');
+
+    const { author, quote } = !!data && data[counter];
   return (
     <>
         <h1>Breaking Bad Quotes</h1>
         <hr/>
+
+        {
+          isLoading ? 
+          <LoadingQuote /> :
+          <Quote author={author} quote={quote} />
+        }
+
+        <button 
+          className='btn btn-primary' 
+          disabled={isLoading}
+          onClick={() => increment()}>
+          Next quote
+        </button>
+
     </>
   )
 }
+
+//La api original no iba
