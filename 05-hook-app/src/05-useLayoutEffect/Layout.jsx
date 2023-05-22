db258f7bc5003806220f6a1906b8ba518d06c713
Abraham <abrahamlega@gmail.com>
2023-05-22 22:08:54 +0100

    136 - Memos y ultimos hooks

---
+++ 05-hook-app/src/05-useLayoutEffect/Layout.jsx
@@ -0,0 +1,33 @@
+import { useFetch, useCounter } from "../hooks";
+import { LoadingQuote, Quote } from "../03-examples";
+
+export const Layout = () => {
+
+    const {counter, increment, decrement, reset} = useCounter(0);
+
+    const { data, isLoading, hasError } = useFetch('https://api.breakingbadquotes.xyz/v1/quotes/10');
+
+    const { author, quote } = !!data && data[counter];
+  return (
+    <>
+        <h1>Breaking Bad Quotes</h1>
+        <hr/>
+
+        {
+          isLoading ? 
+          <LoadingQuote /> :
+          <Quote author={author} quote={quote} />
+        }
+
+        <button 
+          className='btn btn-primary' 
+          disabled={isLoading}
+          onClick={() => increment()}>
+          Next quote
+        </button>
+
+    </>
+  )
+}
+
+//La api original no iba
