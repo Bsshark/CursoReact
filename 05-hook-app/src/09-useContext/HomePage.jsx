121bd6e457e6d620ee8e1b08b079c9369025387e
Abraham <abrahamlega@gmail.com>
2023-06-06 22:31:42 +0100

    162 - Routing y Context completo

---
+++ 05-hook-app/src/09-useContext/HomePage.jsx
@@ -0,0 +1,19 @@
+import { UserContext } from './context/UserContext';
+import { useContext } from 'react'
+
+export const HomePage = () => {
+
+  const { user } = useContext( UserContext);
+
+    return (
+      <>
+          <h1>Home Page <small>{user?.name}</small></h1>
+          <hr />
+
+          <pre>
+          { JSON.stringify(user, null, 3) }
+        </pre>
+      </>
+    )
+  }
+  
---
+++ 05-hook-app/src/09-useContext/LoginPage.jsx
@@ -0,0 +1,23 @@
+import { UserContext } from './context/UserContext';
+import { useContext } from 'react'
+
+export const LoginPage = () => {
+
+  const { user, setUser} = useContext(UserContext);
+
+    return (
+      <>
+        <h1>Login Page</h1>
+        <hr />
+
+        <pre>
+          { JSON.stringify(user, null, 3) }
+        </pre>
+
+        <button className="btn btn-primary"
+          onClick= {() => setUser({id: 123, name: 'Juan', email: 'juan@gmail.com'})} >
+          Establecer usuario
+        </button>
+    </>
+  )
+}
