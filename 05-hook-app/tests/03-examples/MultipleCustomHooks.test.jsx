cf8b3ba0d1ad6ca6b33d5f5435152f3ab9a82a19
Abraham <abrahamlega@gmail.com>
2023-06-14 20:54:01 +0100

    170 - Añadida prueba del MultipleCustomHooks

--- 05-hook-app/tests/03-examples/MultipleCustomHooks.test.jsx
+++ 05-hook-app/tests/03-examples/MultipleCustomHooks.test.jsx
@@ -1,8 +1,30 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import { MultipleCustomHooks } from "../../src/03-examples";
+import { useFetch } from "../../src/hooks/useFetch"; //Centrarse en el archivo para el mock
+import { useCounter } from "../../src/hooks";
 
-describe('Pruebas de MultipleCustomHooks', () => { 
+jest.mock('../../src/hooks/useFetch');
+jest.mock('../../src/hooks/useCounter');
+
+describe('Pruebas de MultipleCustomHooks', () => {
+    const mockIncrement = jest.fn();
+    useCounter.mockReturnValue({
+        counter: 0,
+        increment: mockIncrement
+    })
+
+    beforeEach(() => {
+        jest.clearAllMocks();
+    });
+
     it('should show the component by default', () => {
+
+        useFetch.mockReturnValue({
+            data: null, 
+            isLoading: true, 
+            hasError: null
+        });
+
         render(<MultipleCustomHooks/>);
 
         expect(screen.getByText('Cargando...'));
@@ -13,5 +35,40 @@ describe('Pruebas de MultipleCustomHooks', () => {
 
         screen.debug();
 
+    });
+    it('should show a Quote', () => {
+
+        useFetch.mockReturnValue({
+            data: [{author: 'Abraham', quote: 'Hola Mundo'}], 
+            isLoading: false, 
+            hasError: null
+        });
+
+        render(<MultipleCustomHooks />);
+        expect(screen.getByText('Hola Mundo')).toBeTruthy();
+        expect(screen.getByText('Abraham')).toBeTruthy();
+
+        const nextButton = screen.getByRole('button', {name: 'Next quote'});
+        expect(nextButton.disabled).toBeFalsy();
+
+    });
+
+    it('should call the increment function', () => {
+
+        
+
+        useFetch.mockReturnValue({
+            data: [{author: 'Abraham', quote: 'Hola Mundo'}, {author: 'Abraham2', quote: 'Hola Mundo2'}], 
+            isLoading: false, 
+            hasError: null
+        });
+
+        
+
+        render(<MultipleCustomHooks />);
+        const nextButton = screen.getByRole('button', {name: 'Next quote'});
+        fireEvent.click(nextButton);
+
+        expect(mockIncrement).toHaveBeenCalled();
     });
  })
