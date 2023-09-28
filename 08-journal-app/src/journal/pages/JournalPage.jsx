3915144e3dcfbd788e8505546cd92dff66133645
Abraham <abrahamlega@gmail.com>
2023-09-28 20:54:56 +0100

    250. Added some components to UI

--- 08-journal-app/src/journal/pages/JournalPage.jsx
+++ 08-journal-app/src/journal/pages/JournalPage.jsx
@@ -1,12 +1,14 @@
 import { MailOutline } from "@mui/icons-material"
 import { Typography } from "@mui/material"
+import { JournalLayout } from "../layout/JournalLayout"
 
 export const JournalPage = () => {
   return (
-    <>
-        <Typography variant='h1'>Journal Page</Typography>
-        <MailOutline />
-    </>
+    <JournalLayout>
+      <Typography>
+        Dolor nisi quis sunt duis magna do proident proident irure sint esse.
+      </Typography>
+    </JournalLayout>
     
   )
 }
