8d63ac83a4d9cc79c84951c4142f785e48afae34
Abraham <abrahamlega@gmail.com>
2023-10-05 23:08:11 +0100

    257. Terminado layout de journal app

--- 08-journal-app/src/journal/pages/JournalPage.jsx
+++ 08-journal-app/src/journal/pages/JournalPage.jsx
@@ -1,13 +1,34 @@
-import { MailOutline } from "@mui/icons-material"
-import { Typography } from "@mui/material"
+import { AddOutlined, MailOutline } from "@mui/icons-material"
+import { IconButton, Typography } from "@mui/material"
 import { JournalLayout } from "../layout/JournalLayout"
+import { NoteView, NothingSelectedView } from "../views/"
 
 export const JournalPage = () => {
   return (
     <JournalLayout>
-      <Typography>
+      {/* <Typography>
         Dolor nisi quis sunt duis magna do proident proident irure sint esse.
-      </Typography>
+      </Typography> */}
+      <NothingSelectedView />
+      {/* <NoteView/> */}
+
+      <IconButton 
+        size="large"
+        sx={{
+          color: 'white',
+          bgcolor: 'error.main',
+          ':hover': {
+            bgcolor: 'error.main',
+            opacity: 0.9
+          },
+          position: 'fixed',
+          right: 50,
+          bottom: 50
+        }}
+      >
+        <AddOutlined sx={{fontSize: 30}}/>
+
+      </IconButton>
     </JournalLayout>
     
   )
